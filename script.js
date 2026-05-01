c4e124aa92834bac429c45e1dd915a9f98d8fb73
spenaflorida <spenaflorida@my.bcit.ca>
2026-05-01 11:31:50 -0700

    code: add API button and adjust style

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("APIButton");
+const status = document.getElementById("status");
+
+
+button.addEventListener("click", function () {
+    status.textContent = "Getting Devil Fruit...";
+
+    fetch("https://api.api-onepiece.com/v2/fruits/en/{id]")
+    .then(response => response.json())
+    .then(data => {
+        text.src = data.message;
+        status.textContent = "Your Devil Fruit is...";
+    })
+    .catch(error => console.error("Error getting Devil Fruit: ", error))
+})
